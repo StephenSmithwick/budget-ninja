475a7b79c7348952e2b0d49823f9b0f2003d7ec9
StephenSmithwick <Stephen.Smithwick@gmail.com>
2016-04-16 21:16:15 +1000

    Making sure it renders well on the mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import logo from '../images/ninja_small.png';
 export default class Header extends Component {
   render() {
     return (
-      <h1><img src={logo} alt="ninja logo"/>Budget Ninja</h1>
+      <header><h1><img src={logo} alt="ninja logo"/>Budget Ninja</h1></header>
     );
   }
 }
